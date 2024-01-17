60c9e34b6d848f9c28dc2e9e7b4648946bca4634
Morozov Mikhail <memphis.sail@gmail.com>
2024-01-17 12:27:58 +1000

    add socket.io server in src/index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const path = require("path");
+const { createServer } = require("http");
+const { Server } = require("socket.io");
 const session = require("express-session");
 const mongoose = require("mongoose");
 const cookieParser = require("cookie-parser");
@@ -11,6 +13,8 @@ const adsRouter = require("./routes/adsRouter");
 const chatRouter = require("./routes/chatRouter");
 
 const app = express();
+const httpServer = createServer(app);
+const io = new Server(httpServer, {});
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -24,17 +28,23 @@ app.use("/api/", userRouter);
 app.use("/api/advertisements", adsRouter);
 app.use("/api/chat", chatRouter);
 
+io.on("connection", (socket) => {});
+
 const start = async (port, url) => {
 	await mongoose.connect(url, {
 		dbName: "delivery",
 	});
-	app.listen(port, () => {
-		console.log(`Server started on port ${port}`);
+
+	httpServer.listen(port, () => {
+		console.log(`Server with Socket.io started on port ${port}`);
 	});
+
+	// app.listen(port, () => {
+	// 	console.log(`Server started on port ${port}`);
+	// });
 };
 
 const HTTP_PORT = process.env.HTTP_PORT || 8080;
 const MONGO_EXPRESS_URL =
-	process.env.MONGO_EXPRESS_URL ||
-	"mongodb://mongo:27017/";
+	process.env.MONGO_EXPRESS_URL || "mongodb://mongo:27017/";
 start(HTTP_PORT, MONGO_EXPRESS_URL);
